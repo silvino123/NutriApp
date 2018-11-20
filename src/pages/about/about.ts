73fc3630ba471bfdf0fa2ecaf110e2150a1403a9
silvino123 <36116136+silvino123@users.noreply.github.com>
2018-11-19 19:47:51 -0700

    vv
    vv

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public dieta:string;
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController) {
+  }
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Selecciona un tipo de dieta');
 
+    alert.addInput({
+      type: 'radio',
+      label: 'Equilibrada',
+      value: 'Equilibrada',
+      checked: true
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'Moderada',
+      value: 'Moderada',
+      checked: false
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'Fitness',
+      value: 'Fitness',
+      checked: false
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'Baja en grasas',
+      value: 'Baja en grasas',
+      checked: false
+    });
+
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        // this.testRadioOpen = false;
+         this.dieta = data;
+      }
+    });
+    alert.present();
   }
 
 }
