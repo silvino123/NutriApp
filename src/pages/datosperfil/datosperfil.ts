33e421c9b627e9becb8257aca24e6dd141014336
JMendozaG <JMendozaG00@gmail.com>
2018-12-06 09:38:09 -0600

    ...

--- src/pages/datosperfil/datosperfil.ts
+++ src/pages/datosperfil/datosperfil.ts
@@ -51,10 +51,16 @@ ed:number;
      if(this.perfil.nivel.toString()=="Poco Activo"){
        this.actividadFisica=1.375
     }
-    //this.contador=(66.473+(13.751 + this.pes)+(5.0033*this.al)-(6.7550*this.ed));
-    this.contador=13.751 + this.pes;
-    this.perfil.kcal= this.contador*this.actividadFisica;
-    this.perfil.imc= this.pes/this.al*this.al;
+    if(this.perfil.nivel.toString()=="Medianamente"){
+      this.actividadFisica=1.55
+   }
+   if(this.perfil.nivel.toString()=="Muy Activo"){
+    this.actividadFisica=1.725
+ }
+    this.contador = (66.473 ) + (13.751 * this.pes ) + ( 5.0033 * this.al) - (6.7550 * this.ed);
+    //this.contador=13.751 + this.pes;
+    this.perfil.kcal= this.contador * this.actividadFisica;
+    this.perfil.imc = (this.pes /(this.al*this.al)) * 10000;
     this.perfil.grasa=1.2*this.perfil.imc+0.23*this.ed-10.8*1-5.4;
     this.perfil.agua=2000;
     console.log(this.contador)
@@ -74,6 +80,48 @@ ed:number;
 
      
    }
+   if(this.perfil.sexo.toString()=="Femenino"){
+    this.al= this.perfil.altura;
+    this.pes=this.perfil.peso;
+    this.ed=this.perfil.edad;
+    console.log(this.perfil.sexo)
+    console.log(this.perfil.peso.valueOf())
+    console.log(this.al)
+   if(this.perfil.nivel.toString()=="Sedentario"){
+     this.actividadFisica=1.2
+  }
+  if(this.perfil.nivel.toString()=="Poco Activo"){
+    this.actividadFisica=1.375
+ }
+ if(this.perfil.nivel.toString()=="Medianamente"){
+   this.actividadFisica=1.55
+}
+if(this.perfil.nivel.toString()=="Muy Activo"){
+ this.actividadFisica=1.725
+}
+ this.contador = (655.1 ) + (9.463 * this.pes ) + ( 1.8 * this.al) - (4.6756 * this.ed);
+ //this.contador=13.751 + this.pes;
+ this.perfil.kcal= this.contador * this.actividadFisica;
+ this.perfil.imc= (this.pes / (this.al * this.al)) * 10000;
+ this.perfil.grasa=1.2*this.perfil.imc+0.23*this.ed-10.8*1-5.4;
+ this.perfil.agua=2000;
+ console.log(this.contador)
+ console.log(this.perfil.kcal)
+ console.log(this.perfil.grasa)
+ console.log(this.perfil.imc)
+  this.afDatabase.object(`Perfil/${auth.uid}`).set(this.perfil).then(()=> 
+
+   this.navCtrl.setRoot(TabsPage))
+   //this.KcalHombre();
+   const loader = this.loadingCtrl.create({
+ content: "Enviando...",
+ duration: 800
+});
+loader.present(); 
+
+
+  
+}
     }) 
   
  
