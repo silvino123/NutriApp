09ed5ebaf9ce11f2c0778ccaf390bb4a74b75b14
JMendozaG <JMendozaG00@gmail.com>
2018-12-06 03:12:00 -0600

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleLightContent();
+      statusBar.backgroundColorByHexString("#EF5350");
       splashScreen.hide();
+    });
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+     
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.platform.registerBackButtonAction(() => {
+        this.nav.setRoot(TabsPage);
+});
     });
   }
 }
